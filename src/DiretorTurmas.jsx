4060474486dbf1dd84ddc12e6714b8d4d3f85585
paulorlopespr-source <paulorlopes.pr@gmail.com>
2026-08-12 10:34:45 -0300

    Atualiza identidade para SIEDU-PINDOBAÇU

--- src/DiretorTurmas.jsx
+++ src/DiretorTurmas.jsx
@@ -8,8 +8,8 @@ function DiretorAreaLayout({ user, onLogout, children }) {
     <div className="director-area">
       <aside className="director-area-sidebar">
         <div className="director-area-brand">
-          <img src="/images/sigepin.png" alt="SIEDU" />
-          <strong>SIEDU</strong>
+          <img src="/images/sigepin.png" alt="SIEDU-PINDOBAÇU" />
+          <strong>SIEDU-PINDOBAÇU</strong>
           <small>Sistema Integrado de Educação</small>
         </div>
 
